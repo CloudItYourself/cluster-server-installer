851e9ee577e2c3f9cf1b78006aa2c1055e57e2eb
ronen4822 <ronen4822@gmail.com>
2024-01-18 15:50:20 +0200

    -maybe now?

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="cluster-server-installer",
     version="0.0.1",
     include_package_data=True,
-    package_data={'': ['**.tgz']},
+    package_data={'': ['cluster_server_installer/resources/tailscale/*.tgz']},
     packages=find_packages(),
     install_requires=['kubernetes', 'requests']  # Todo: load requirements.txt
 )
