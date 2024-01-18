c14b54f98920ce5452001ace8252e65237002550
ronen4822 <ronen4822@gmail.com>
2024-01-18 15:45:48 +0200

    -fix setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="cluster-server-installer",
     version="0.0.1",
+    include_package_data=True,
+    package_data={'': ['**.tgz']},
     packages=find_packages(),
     install_requires=['kubernetes', 'requests']  # Todo: load requirements.txt
 )
