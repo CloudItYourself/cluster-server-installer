9fb184ee7c0ae40edbea3fe7202be47c2cccbc85
ronen4822 <ronen4822@gmail.com>
2024-02-01 16:23:11 +0200

    -maybe?

--- cluster_server_installer/certificates/lego_certificate_installer.py
+++ cluster_server_installer/certificates/lego_certificate_installer.py
@@ -8,6 +8,7 @@ from typing import Final, Tuple
 class LegoCertificateInstaller:
     LEGO_FILE_PATH: Final[pathlib.Path] = pathlib.Path(__file__).parent.parent / 'resources' / 'cert_provider' / 'lego'
     DNS_TIMEOUT: Final[int] = 10 * 60
+    CERT_DIR: Final[pathlib.Path] = pathlib.Path('/usr/local/src/certs')
 
     def __init__(self, go_daddy_access_key: str, go_daddy_secret_key: str, email: str, domain: str):
         LegoCertificateInstaller.LEGO_FILE_PATH.chmod(0o777)
@@ -15,26 +16,23 @@ class LegoCertificateInstaller:
         self._secret_key = go_daddy_secret_key
         self._email = email
         self._domain = domain
+        LegoCertificateInstaller.CERT_DIR.mkdir(parents=True, exist_ok=True)
 
     def install_certificates(self) -> bool:
         command = f'GODADDY_API_KEY={self._access_key} \
         GODADDY_API_SECRET={self._secret_key} \
-        {LegoCertificateInstaller.LEGO_FILE_PATH} --dns-timeout {LegoCertificateInstaller.DNS_TIMEOUT} --email {self._email} --dns godaddy --domains {self._domain}'
-        cert_installed = os.system(command + " run") == 0
+        GODADDY_PROPAGATION_TIMEOUT={LegoCertificateInstaller.DNS_TIMEOUT} {LegoCertificateInstaller.LEGO_FILE_PATH} --path {str(LegoCertificateInstaller.CERT_DIR.absolute())} --email {self._email} --dns godaddy --domains {self._domain} --accept-tos'
+        cert_installed = os.system(command + " new") == 0
         if not cert_installed:
             return False
 
-
         cron = crontab.CronTab(user='root')  # replace 'root' with your username if needed
         job = cron.new(command=command + " renew")
-        job.minute.every(1)  # replace with desired frequency
         job.from_line(f'{random.randrange(0, 59)} {random.randrange(0, 23)} 20 * *')
         cron.write()
 
         return True
 
     def get_certificate_root_path(self) -> Tuple[pathlib.Path, pathlib.Path]:
-        (pathlib.Path('.') / '.lego' / f'{self._domain}.crt').chmod(0o777)
-        (pathlib.Path('.') / '.lego' / f'{self._domain}.key').chmod(0o777)
-        return (pathlib.Path('.') / '.lego' / f'{self._domain}.crt',
-                pathlib.Path('.') / '.lego' / f'{self._domain}.key')
+        return (LegoCertificateInstaller.CERT_DIR / 'certificates' / f'{self._domain}.crt',
+                LegoCertificateInstaller.CERT_DIR / 'certificates' / f'{self._domain}.key')
