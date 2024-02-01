86e1c8e67951abfd62aa9e3c546bdf9895020e92
ronen4822 <ronen4822@gmail.com>
2024-02-01 12:41:38 +0200

    -fix main

--- cluster_server_installer/main.py
+++ cluster_server_installer/main.py
@@ -6,10 +6,10 @@ from cluster_server_installer.utilities.logging import initialize_logger
 from cluster_server_installer.vpn.vpn_installer import VpnServerInstaller
 
 
-def main(host_url: str, email: str, registry: str, access_key: str):
+def main(host_url: str, email: str, registry: str, access_key: str, go_daddy_access_key: str, go_daddy_secret: str):
     initialize_logger(LOGGER_NAME)
     vpn_installer = VpnServerInstaller()
-    vpn_installer.install_vpn(host_url=host_url)
+    vpn_installer.install_vpn(host_url=host_url,email=email, godaddy_key=go_daddy_access_key, godaddy_secret=go_daddy_secret)
 
     k3s_installer = K3sInstaller()
     k3s_installer.install_kubernetes(host_url=host_url, email=email, registry_url=registry, access_key=access_key)
@@ -23,5 +23,7 @@ if __name__ == '__main__':
     parser.add_argument('email', type=str)
     parser.add_argument('registry', type=str)
     parser.add_argument('access_key', type=str)
+    parser.add_argument('godaddy_access_key', type=str)
+    parser.add_argument('godaddy_secret', type=str)
     args = parser.parse_args()
-    main(args.server_url, args.email, args.registry, args.access_key)
+    main(args.server_url, args.email, args.registry, args.access_key, args.godaddy_access_key, args.godaddy_secret)
