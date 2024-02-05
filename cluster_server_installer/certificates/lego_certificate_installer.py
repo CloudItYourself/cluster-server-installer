4d4149955d4afac1dbe9371bc5e1baa6b9486a4f
ronen4822 <ronen4822@gmail.com>
2024-02-05 15:32:24 +0200

    -add renew certificate thingy

--- cluster_server_installer/certificates/lego_certificate_installer.py
+++ cluster_server_installer/certificates/lego_certificate_installer.py
@@ -1,8 +1,8 @@
 import os
 import pathlib
+import json
+import sys
 import random
-from tempfile import TemporaryDirectory
-
 import crontab
 from typing import Final, Tuple
 
@@ -12,6 +12,7 @@ class LegoCertificateInstaller:
     DNS_TIMEOUT: Final[int] = 10 * 60
     CERT_DIR: Final[pathlib.Path] = pathlib.Path('/usr/local/src/certs')
     CERT_ORIGINAL_DIR: Final[pathlib.Path] = pathlib.Path('/usr/local/src/orig_certs')
+    CERT_DETAILS_FILE: Final[pathlib.Path] = pathlib.Path('/usr/local/src/orig_certs') / 'renewal_details.json'
     CERT_RETRY_COUNT: Final[int] = 5
 
     def __init__(self, go_daddy_access_key: str, go_daddy_secret_key: str, email: str, domain: str):
@@ -36,27 +37,56 @@ class LegoCertificateInstaller:
 
         if not cert_installed:
             return False
-        
-        headscale_cert_path = LegoCertificateInstaller.CERT_DIR / 'certificates' / f'{self._domain}.crt'
-        headscale_key_path = LegoCertificateInstaller.CERT_DIR / 'certificates' / f'{self._domain}.key'
-        headscale_cert_path.parent.mkdir(parents=True, exist_ok=True)
 
-        headscale_cert_path.write_bytes(
-            (LegoCertificateInstaller.CERT_ORIGINAL_DIR / 'certificates' / f'{self._domain}.crt').read_bytes())
-        headscale_key_path.write_bytes(
-            (LegoCertificateInstaller.CERT_ORIGINAL_DIR / 'certificates' / f'{self._domain}.key').read_bytes())
+        renewal_details = {
+            'GODADDY_API_KEY': self._access_key,
+            'GODADDY_API_SECRET': self._secret_key,
+            'EMAIL': self._email,
+            'DOMAIN': self._domain
+        }
 
-        headscale_cert_path.chmod(0o777)
-        headscale_key_path.chmod(0o777)
-        return cert_installed
+        LegoCertificateInstaller.CERT_DETAILS_FILE.write_text(json.dumps(renewal_details))
+        LegoCertificateInstaller.alter_certificate_permissions(self._domain)
 
-        # cron = crontab.CronTab(user='root')  # replace 'root' with your username if needed
-        # job = cron.new(command=command + " renew")
-        # job.from_line(f'{random.randrange(0, 59)} {random.randrange(0, 23)} 20 * *')
-        # cron.write()
-        #
-        # return True
+        cron = crontab.CronTab(user='root')
+        job = cron.new(command=f'{str(pathlib.Path(sys.executable).absolute())} renew-certs')
+        job.from_line(f'{random.randrange(0, 59)} {random.randrange(0, 23)} 20 * *')
+        cron.write()
+
+        return True
 
     def get_certificate_root_path(self) -> Tuple[pathlib.Path, pathlib.Path]:
         return (LegoCertificateInstaller.CERT_DIR / 'certificates' / f'{self._domain}.crt',
                 LegoCertificateInstaller.CERT_DIR / 'certificates' / f'{self._domain}.key')
+
+    @staticmethod
+    def alter_certificate_permissions(domain_name: str):
+        headscale_cert_path = LegoCertificateInstaller.CERT_DIR / 'certificates' / f'{domain_name}.crt'
+        headscale_key_path = LegoCertificateInstaller.CERT_DIR / 'certificates' / f'{domain_name}.key'
+        headscale_cert_path.parent.mkdir(parents=True, exist_ok=True)
+
+        headscale_cert_path.write_bytes(
+            (LegoCertificateInstaller.CERT_ORIGINAL_DIR / 'certificates' / f'{domain_name}.crt').read_bytes())
+        headscale_key_path.write_bytes(
+            (LegoCertificateInstaller.CERT_ORIGINAL_DIR / 'certificates' / f'{domain_name}.key').read_bytes())
+
+        headscale_cert_path.chmod(0o777)
+        headscale_key_path.chmod(0o777)
+
+    @staticmethod
+    def renew_certificates():
+        cert_renewed = False
+        cert_details = json.loads(LegoCertificateInstaller.CERT_DETAILS_FILE.read_text())
+        command = f'GODADDY_API_KEY={cert_details["GODADDY_API_KEY"]} \
+        GODADDY_API_SECRET={cert_details["GODADDY_API_SECRET"]} \
+        GODADDY_PROPAGATION_TIMEOUT={LegoCertificateInstaller.DNS_TIMEOUT} {LegoCertificateInstaller.LEGO_FILE_PATH} --path {str(LegoCertificateInstaller.CERT_ORIGINAL_DIR.absolute())} --email {cert_details["EMAIL"]} --dns godaddy --domains {cert_details["DOMAIN"]} --accept-tos'
+        for i in range(LegoCertificateInstaller.CERT_RETRY_COUNT):
+            cert_renewed = os.system(command + " renew") == 0
+            if cert_renewed:
+                break
+
+        if not cert_renewed:
+            return False
+
+        LegoCertificateInstaller.alter_certificate_permissions(cert_details["DOMAIN"])
+        return True
--- cluster_server_installer/main.py
+++ cluster_server_installer/main.py
@@ -1,6 +1,7 @@
 import argparse
 
 from cluster_server_installer import LOGGER_NAME
+from cluster_server_installer.certificates.lego_certificate_installer import LegoCertificateInstaller
 from cluster_server_installer.k8s.k3s_installer import K3sInstaller
 from cluster_server_installer.utilities.logging import initialize_logger
 from cluster_server_installer.vpn.vpn_installer import VpnServerInstaller
@@ -19,11 +20,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog='CloudIY Server Installer',
         description='Installs VPN and K3S')
-    parser.add_argument('server_url', type=str)
-    parser.add_argument('email', type=str)
-    parser.add_argument('registry', type=str)
-    parser.add_argument('access_key', type=str)
-    parser.add_argument('godaddy_access_key', type=str)
-    parser.add_argument('godaddy_secret', type=str)
+    subparsers = parser.add_subparsers(dest='command')
+    install_parser = subparsers.add_parser('install')
+    install_parser.add_argument('server_url', type=str)
+    install_parser.add_argument('email', type=str)
+    install_parser.add_argument('registry', type=str)
+    install_parser.add_argument('access_key', type=str)
+    install_parser.add_argument('godaddy_access_key', type=str)
+    install_parser.add_argument('godaddy_secret', type=str)
+
+    subparsers.add_parser('renew-certs')
     args = parser.parse_args()
-    main(args.server_url, args.email, args.registry, args.access_key, args.godaddy_access_key, args.godaddy_secret)
+
+    if args.command == 'install':
+        main(args.server_url, args.email, args.registry, args.access_key, args.godaddy_access_key, args.godaddy_secret)
+    elif args.command == 'renew-certs':
+        LegoCertificateInstaller.renew_certificates()
+    else:
+        raise Exception(f"Undefined command: {args.command}")
