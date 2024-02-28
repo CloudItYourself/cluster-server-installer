6332bcea9fe3e114d0a1d863e45697fad1dd2e89
ronen4822 <ronen4822@gmail.com>
2024-02-28 08:49:54 +0200

    -give the correct setup

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="cluster-server-installer",
-    version="0.0.1",
+    version="1.0.1",
     include_package_data=True,
     package_data={'': ['resources/tailscale/*.tgz', 'resources/deployments/certificates/*.yaml',
                        'resources/deployments/dashboard/*.yaml', 'resources/deployments/ciy/*.yaml',
+                       'resources/deployments/storage/*.yaml', 'resources/deployments/database/*.yaml',
                        'resources/cert_provider/*']},
     packages=find_packages(),
-    install_requires=['kubernetes', 'requests', 'python-crontab']  # Todo: load requirements.txt
+    install_requires=['kubernetes', 'requests', 'python-crontab']
 )
